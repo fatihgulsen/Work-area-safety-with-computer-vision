7269b9d8a5203965c13bdc7b91c049267df8a00a
fatihgulsen <fatihgulsen@outlook.com.tr>
2022-05-19 12:55:36 +0300

    Script & del ipnb

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from SafetyArea import *
+import argparse
 
-detector = SafetyArea(video_dir='SampleVideo/securityvideo1.mp4', GPU=True)
-# detector.setup_area()
-detector.video_detection(Keypoints=True, Mask=False)
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--type", type=str, help="Setup or Run")
+parser.add_argument("-v", "--video", type=str, help="Video dir")
+parser.add_argument("-m", "--mask", type=int, choices=[0, 1], help="Mask on =1 mask off=0")
+args = parser.parse_args()
+
+
+def main():
+    detector = SafetyArea(video_dir=args.video, GPU=True)
+    if args.type == 'setup':
+        detector.setup_area()
+    elif args.type == 'run':
+        if args.mask == 1:
+            detector.video_detection(Keypoints=True, Mask=True)
+        elif args.mask == 0:
+            detector.video_detection(Keypoints=True, Mask=False)
+
+
+main()
--- maskrcnn.py
+++ maskrcnn.py
@@ -13,7 +13,8 @@ import time
 import os
 
 COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    '__background__', 'person'
+    , 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
     'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
     'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
